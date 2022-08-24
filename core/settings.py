66e1a2323f01d70c2a4547573bf725f59bc5738b
ebuks1993 <chukwuka932@gmail.com>
2022-08-24 14:51:27 +0100

    core

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&91ax!6x2g7c!cw-_3dc#by3svsfaks7fly7tog8ag-ahny+18
 # DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = ['54.91.86.27','*']
+ALLOWED_HOSTS = ["18.132.246.93",'*']
 
 
 # Application definition
